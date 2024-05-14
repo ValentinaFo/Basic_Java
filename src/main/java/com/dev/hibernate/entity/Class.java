a47ede425f98c0592965ebbb99f35efd819d051b
ValentinaFo <160612530+ValentinaFo@users.noreply.github.com>
2024-05-14 15:32:23 +0200

    esercizio Hibernate

---
+++ src/main/java/com/dev/hibernate/entity/Class.java
@@ -0,0 +1,51 @@
+package com.dev.hibernate.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "classes")
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/dev/hibernate/entity/Enrollment.java
@@ -0,0 +1,35 @@
+package com.dev.hibernate.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enrollments")
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+
+}
+
+
