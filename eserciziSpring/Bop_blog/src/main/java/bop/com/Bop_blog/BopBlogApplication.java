e1a15100b753f62e89b52b2896d1ced62ff8a732
ValentinaFo <160612530+ValentinaFo@users.noreply.github.com>
2024-05-15 12:51:09 +0200

    esercizio mock-h2

---
+++ eserciziSpring/Bop_blog/src/main/java/bop/com/Bop_blog/BopBlogApplication.java
@@ -0,0 +1,13 @@
+package bop.com.Bop_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BopBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BopBlogApplication.class, args);
+	}
+
+}
---
+++ eserciziSpring/Bop_blog/src/test/java/bop/com/Bop_blog/BopBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package bop.com.Bop_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BopBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
