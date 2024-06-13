776b04703e59e3f8d5bd6098169be65e2dd2559b
ValentinaFo <160612530+ValentinaFo@users.noreply.github.com>
2024-06-13 16:40:29 +0200

    change environment

--- eserciziSpring/EnvironmentVariables2/src/main/java/co/develhope/EnvironmentVariables2/Controller.java
+++ eserciziSpring/EnvironmentVariables2/src/main/java/co/develhope/EnvironmentVariables2/Controller.java
@@ -1,2 +1,20 @@
-package co.develhope.EnvironmentVariables2;public class Controller {
+package co.develhope.EnvironmentVariables2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String welcome(){
+        String benvenuto = environment.getProperty("welcomeMsg");
+        return benvenuto;
+    }
 }
